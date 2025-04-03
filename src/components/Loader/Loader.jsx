8768a969919cf51f7448a2611ebc97e2420effad
stanislav_nedelchev <stasnedel@gmail.com>
2025-04-03 20:30:53 +0300

    add pretiierrc.json, local fonts and created pages

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import { Circles } from 'react-loader-spinner';
+const Loader = () => {
+  return (
+    <Circles
+      height="80"
+      width="80"
+      color="var(--button)"
+      ariaLabel="circles-loading"
+      wrapperStyle={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+      wrapperClass=""
+      visible={true}
+    />
+  );
+};
+export default Loader;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { persistor, store } from './redux/store.js';
+import App from './App.jsx';
+import 'modern-normalize';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <Provider store={store}> */}
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <App />
+      {/* </PersistGate> */}
+      {/* </Provider> */}
+    </BrowserRouter>
   </StrictMode>,
-)
+);
